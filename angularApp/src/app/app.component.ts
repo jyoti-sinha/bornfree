e53ec463af43fb73d2b3fab1b155e9c8e1eb5fd1
Jyoti Sinha <JS522874@GSK.SATYAMBPO.COM>
2019-06-17 15:06:08 +0530

    updated app component

--- angularApp/src/app/app.component.ts
+++ angularApp/src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularApp';
+  seats: Array<any> = [];
+  female_seats: Array<number> = [5, 6, 7, 8, 17, 18, 19, 20];
+
+  constructor(){
+    for(let i=0; i<24; i++){
+      this.seats.push({
+        _id: i,
+        F_seat: false,
+        booked: false
+      });
+    }
+    this.seats.forEach((el, index) => {
+      this.female_seats.forEach((item, i) => {
+        if(index == item){
+          el.F_seat = true;
+        }
+      })
+    })
+    console.log(this.seats)
+  }
 }
