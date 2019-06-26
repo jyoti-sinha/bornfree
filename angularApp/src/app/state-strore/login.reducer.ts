392dad7f38b01c953ae22590e77503a2fa716822
jyoti-sinha <42303797+jyoti-sinha@users.noreply.github.com>
2019-06-26 13:00:07 +0530

    Delete login.reducer.ts

--- angularApp/src/app/state-strore/login.reducer.ts
+++
@@ -1,9 +0,0 @@
-import { createReducer, on } from '@ngrx/store';
-import { actionTrue, actionFalse } from './login.action';
-
-export const stateTrue = sessionStorage.getItem('token') ? false : true;
-
-export const loginReducer = createReducer(stateTrue, 
-    on(actionTrue, s => s = true),
-    on(actionFalse, s => s = false)
-);
